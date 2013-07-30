62fce707a0843a259b57da905aae552ee146bd3d
Peter Thiemann <peter.thiemann@gmail.com>
2013-07-30 15:48:26 +0200

    refactored

--- src/Grammar.hs
+++ src/Grammar.hs
@@ -19,8 +19,14 @@ data RCFG n t
 liftG :: CFG n t -> CFG (n,[t]) t
 liftG (CFG nts ts ps start) =
     CFG (map liftNT nts) ts (map liftProd ps) (liftNT start)
-    where
-    liftNT n = (n,[])
-    liftProd (Production n alpha) = Production (liftNT n) (map liftSymbol alpha)
-    liftSymbol (Left n)  = Left (liftNT n)
-    liftSymbol (Right t) = Right t
+
+liftNT n = (n,[])
+liftProd (Production n alpha) = Production (liftNT n) (map liftSymbol alpha)
+liftSymbol (Left n)  = Left (liftNT n)
+liftSymbol (Right t) = Right t
+
+liftRG :: RCFG n t -> RCFG (n, [t]) t
+liftRG rcfg =
+    RCFG { cfg = liftG (cfg rcfg), useful = map liftNT (useful rcfg), nullable = map liftNT (nullable rcfg) }
+
+    
