e868fdd1c896505e38706335503d3462229d8155
Peter Thiemann <peter.thiemann@gmail.com>
2015-11-30 15:35:12 +0100

    generation of counterexamples added

--- src/Equivalence.hs
+++ src/Equivalence.hs
@@ -18,6 +18,7 @@ examples = [
          (Atom 'a', Atom 'a', True),
          (Atom 'b', Atom 'c', False),
          (Star (Atom 'a'), Alt One (Dot (Atom 'a') (Star (Atom 'a'))), True), -- a* = 1+aa*
+         (Star (Atom 'a'), Alt One (Dot (Atom 'a') (Star (Atom 'b'))), False), -- a* != 1+ab*
          (Star (Alt (Atom 'a') (Atom 'b')), Star (Dot (Star (Atom 'a')) (Star (Atom 'b'))), True) -- (a+b)* = (a*b*)*
          ]
 
@@ -27,3 +28,29 @@ runExamples sigma ((r,s,result) : rest) =
             (equiv sigma r s == result) && runExamples sigma rest
 
 test = runExamples "abc" examples
+
+-- with counterexample
+
+data Result t = Yes | No [t]
+     deriving   Show
+
+equivc :: Ord t => [t] -> RE t -> RE t -> Result t
+equivc sigma r s = rewritec sigma [] [(r, s)] [[]]
+
+rewritec :: Ord t => [t] -> [(RE t, RE t)] -> [(RE t, RE t)] -> [[t]] -> Result t
+rewritec sigma seen [] [] = Yes
+rewritec sigma seen (p : worklist) (path : rest) | p `elem` seen = 
+         rewritec sigma seen worklist rest
+rewritec sigma seen (p@(r, s) : worklist) (path : rest) =
+         if nullable r /= nullable s
+         then No path
+         else rewritec sigma (p : seen)
+                             ([(derivative t r, derivative t s) | t <- sigma] ++ worklist)
+                             (map (: path) sigma ++ rest)
+
+runExamplesc :: Ord t => [t] -> [(RE t, RE t, Bool)] -> [Result t]
+runExamplesc sigma [] = []
+runExamplesc sigma ((r,s,result) : rest) = 
+            equivc sigma r s : runExamplesc sigma rest
+
+testc = runExamplesc "abc" examples
